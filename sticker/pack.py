7ca53e1c62e152ab5a1fd13e1c0235610c35adeb
Johny404 <vicha.jan@post.cz>
2022-10-05 22:20:41 +0200

    fixes

--- sticker/pack.py
+++ sticker/pack.py
@@ -79,7 +79,7 @@ async def upload_sticker(file: str, directory: str, old_stickers: Dict[str, matr
     else:
         preview_image_data, width, height = util.convert_image(image_data)
         print(".", end="", flush=True)
-        mxc = await matrix.upload(image_data, "image/gif", file)
+        mxc = await matrix.upload(image_data, "image/png", file)
         mxco = await matrix.upload(preview_image_data, "image/png", file)
         print(".", end="", flush=True)
         sticker = util.make_sticker(mxc, width, height, len(image_data), name)
